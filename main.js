1e232c01ee6b5355d65e68ec09cd1200cc758c0a
jipark2 <71774125+jipark2@users.noreply.github.com>
2020-12-17 22:07:40 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var playpause = anime({
+    targets: 'div.box',
+    translateY: [
+      { value: 200, duration: 500 },
+      { value: 0, duration: 800 }
+    ],
+    rotate:{
+        value:'1turn',
+        easing: 'easeInOutSine'
+    },
+    opacity: '0.7',
+    delay: function(el, i, l){ return i * 1000},
+    autoplay: false,
+    loop: true
+  });
+
+document.querySelector('#boxes .play').onclick = playpause.play;
+document.querySelector('#boxes .pause').onclick = playpause.pause;
